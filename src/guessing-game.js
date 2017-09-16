a071647b79048c5d617af94de5dc3979bf5d355a
Nikita Mikhaliuk <nikkita1992@mail.ru>
2017-09-16 19:18:34 +0300

    1st solution, based on primary binary search algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,28 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.guessArr = [];
 
+        for (let i = min; i <= max; i++) {
+            this.guessArr.push(i);
+        }
+        this.LeftBorder = 0;
+        this.RightBorder = this.guessArr.length - 1;
     }
 
     guess() {
 
+        let guessIndex = Math.ceil((this.LeftBorder + this.RightBorder) / 2);
+        this.lastGuessIndex = guessIndex;
+        return this.guessArr[guessIndex];
     }
 
     lower() {
-
+        this.RightBorder = this.lastGuessIndex;
     }
 
     greater() {
-
+        this.LeftBorder = this.lastGuessIndex;
     }
 }
 
